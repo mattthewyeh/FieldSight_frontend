3c70d647c589ac57a547ddfc3effd7a48d60a2e6
Tanisha Jain <tanishajain@Tanishas-MacBook-Air.local>
2026-03-11 20:03:59 -0700

    basic setup of react with vite and create dashboard page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Dashboard from './Dashboard';
+
+function App() {
+  // We initialize it to false because the session hasn't started yet
+  const [isSessionActive, setIsSessionActive] = useState(false);
+  const handleStartSession = () => {
+  setIsSessionActive(true); 
+};
+
+  return (
+  <div className="app-container">
+    {isSessionActive ? (
+      <Dashboard />
+    ) : (
+      <div className="start-screen">
+        <button onClick={handleStartSession}>
+          Start Session
+        </button>
+      </div>
+    )}
+  </div>
+);
+}
+
+export default App
